3d190a81a242c7a45fdfe891ae2b54b74d8e5638
Ajlan Khan <105883399+ajlank@users.noreply.github.com>
2022-06-05 22:40:18 +0600

    Lines have Updated..

--- myapp/src/components/Country.js
+++ myapp/src/components/Country.js
@@ -5,10 +5,10 @@ function Country(props){
  return <article>
      <div className={style.country}>
          <img src={flags.png} alt={name.common}/>
-         <h4>Name:{name.common}</h4>
-         <h4>Capital:{capital}</h4>
-         <h4>Population:{population}</h4>
-         <h4>area:{area} sq</h4>
+         <h4>Name: {name.common}</h4>
+         <h4>Capital: {capital}</h4>
+         <h4>Population: {population}</h4>
+         <h4>area: {area} sq</h4>
          <button className={style.btn} onClick={()=>{
             props.onRemoveCountry(name.common);
          }}>Remove Country</button>
